ce84c5b452a8afff10a303323e6e562ad04e6670
tingweiouo <kklb1716@gmail.com>
2023-11-22 18:48:29 +0800

    first commit

---
+++ radar_localization/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tingweiou/radar_localization/src
+# Build directory: /home/tingweiou/radar_localization/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
