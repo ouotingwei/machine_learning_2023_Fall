8f886b70805e82aa764f10203ab9397d3a0870a7
tingweiou <kklb1716@gmail.com>
2023-12-21 20:48:18 +0800

    add hw5-sfs

--- hw5_SFS_FC/hw5..py
+++ hw5_SFS_FC/hw5..py
@@ -1,21 +1,23 @@
 from sklearn.datasets import load_breast_cancer
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import auc
 from sklearn.model_selection import train_test_split
-from collections import defaultdict
 import lda 
 
 # SFS
 def sequential_forward_selection_2fold(X, y):
+
+    print("------------------------SFS-----------------------------")
+
     num_features = X.shape[1]
     selected_features = []
+    accuracies = []
     best_accuracy = 0
+    best_loc = 0
 
-    for _ in range(num_features):   # 30
+    for i in range(num_features):   # 30
         remaining_features = [feature for feature in range(num_features) if feature not in selected_features]
         local_best_feature = None
+        local_best_accuracy = 0
 
         for feature in remaining_features:
             current_features = selected_features + [feature]
@@ -24,36 +26,49 @@ def sequential_forward_selection_2fold(X, y):
             current_features = [feature for feature in current_features if feature is not None]
             fold1_X, fold2_X, fold1_y, fold2_y = train_test_split(X[:, np.array(current_features).astype(int)], y, test_size=0.5, random_state=0)
 
-
             # Evaluate the model on the first fold
             lda_ = lda.LDA()
             lda_.fit(fold1_X, fold1_y)
-            fold_1_accuracy = lda_.LDA_decision_function(fold2_X, fold2_y)*100
+            fold_1_accuracy = lda_.LDA_decision_function(fold2_X, fold2_y) * 100
 
             # Evaluate the model on the second fold
             lda_.fit(fold2_X, fold2_y)
-            fold_2_accuracy = lda_.LDA_decision_function(fold1_X, fold1_y)*100
+            fold_2_accuracy = lda_.LDA_decision_function(fold1_X, fold1_y) * 100
 
             # Calculate the average accuracy
-            accuracy = (fold_1_accuracy + fold_2_accuracy)/2
+            accuracy = (fold_1_accuracy + fold_2_accuracy) / 2
 
-            if accuracy > best_accuracy:
-                best_accuracy = accuracy
+            if accuracy > local_best_accuracy:
+                local_best_accuracy = accuracy
                 local_best_feature = feature
 
-        selected_features.append(local_best_feature)
-        print(f"Step {_ + 1}: Selected Feature {local_best_feature}, Accuracy: {best_accuracy}")
+        if local_best_feature is not None:
+            selected_features.append(local_best_feature)
+            accuracies.append(local_best_accuracy)
+        
+        if local_best_accuracy > best_accuracy:
+            best_accuracy = local_best_accuracy
+            best_loc = i
+
+        print(f" Step {i + 1}: Selected Feature {local_best_feature}, Accuracy: {local_best_accuracy}")
+
+    print(" [-] Best Accuracy:", best_accuracy)
+    print(" [-] Features with the highest accuracy (indices):", selected_features[:best_loc])
+
+    return selected_features, accuracies
 
 
-    return selected_features
+def fisher_criterion_2fold(X, y):
+    print("------------------------Fisher's Criterion-----------------------------")
+
 
 def main():
     cancer = load_breast_cancer()
     x = cancer.data
     y = cancer.target
-    print(x.shape)
 
     sequential_forward_selection_2fold(x, y)
+    fisher_criterion_2fold(x, y)
 
 
 if __name__ == '__main__':
--- hw5_SFS_FC/lda.py
+++ hw5_SFS_FC/lda.py
@@ -3,12 +3,7 @@
 date : 10-8-2023
 Machien Learning HW2 ( NYCU FALL-2023 )
 """
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import auc
-from collections import defaultdict
-
 
 class LDA():
     def __init__(self):
@@ -38,11 +33,11 @@ class LDA():
             covariance = np.var(class_1) * p1 + np.var(class_2) * p2
 
             w_T = (mean_class_1 - mean_class_2) / covariance
-            b = -0.5 * w_T * (mean_class_1 + mean_class_2) - np.log(C * (p2 / p1))
+            self.b = -0.5 * w_T * (mean_class_1 + mean_class_2) - np.log(C * (p2 / p1))
 
             w_T = np.array([w_T])  # Ensure w_T is a vector
             w_T = np.round(w_T, 2)
-            b = round(b, 2)
+            self.b = round(self.b, 2)
 
         else:
             self.mean_class_1 = np.mean(class_1, axis=0)
@@ -53,19 +48,16 @@ class LDA():
             covariance = covariance_1 * p1 + covariance_2 * p2
 
             w_T = (self.mean_class_1 - self.mean_class_2).T @ np.linalg.inv(covariance)
-            b = -0.5 * w_T @ (self.mean_class_1 + self.mean_class_2) - np.log(C * (p2 / p1))
+            self.b = -0.5 * w_T @ (self.mean_class_1 + self.mean_class_2) - np.log(C * (p2 / p1))
 
             w_T = np.round(w_T, 2)
-            b = round(b, 2)
+            self.b = round(self.b, 2)
 
         self.w_T = np.array([w_T])  # Ensure w_T is a vector
-        b = 0 if x.shape[1] == 1 else b  # Set a default value for b if using multiple features
+        self.b = 0 if x.shape[1] == 1 else self.b  # Set a default value for b if using multiple features
 
-        return w_T, b
+        return w_T, self.b
 
-
-
-        
 
     def LDA_decision_function(self, x, y_true):
         TP = FP = FN = TN = 0
@@ -73,8 +65,10 @@ class LDA():
         self.predicted_list = []
 
         if x.ndim == 1:  # If there is only one feature
+            
             for i in range(len(x)):
                 g = self.w_T * x[i] + self.b
+
                 predicted_class = 1 if g > 0 else 0
                 self.predicted_list.append(predicted_class)
 
@@ -86,12 +80,13 @@ class LDA():
                     FP += 1
                 if predicted_class != y_true[i] and predicted_class == 0:
                     FN += 1
-
+        
         else:  # If there are multiple features
             for i in range(len(x)):
                 x_col = x[i].T
                 if self.b is not None:  # Check if self.b is not None
                     g = self.w_T @ x_col + self.b
+
                     predicted_class = 1 if g > 0 else 0
                     self.predicted_list.append(predicted_class)
 
@@ -103,24 +98,15 @@ class LDA():
                         FP += 1
                     if predicted_class != y_true[i] and predicted_class == 0:
                         FN += 1
-
-        # Add a condition to check if the denominator is zero
-        if TP + FN != 0:
-            self.TPR = TP / (TP + FN)
-        else:
-            self.TPR = 0
-
-        #self.FPR = FP / (FP + TN)
-
+    
         accuracy = (TP + TN) / len(y_true)
 
         return accuracy
 
-
-
     
     def return_TPR_AND_FPR(self):
         return self.TPR, self.FPR
+    
     
     def return_predicted_list(self):
         return self.predicted_list
