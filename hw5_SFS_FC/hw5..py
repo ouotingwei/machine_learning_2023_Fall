f6598efe61b62ae05106f9127241807a3c5d12bb
tingweiou <kklb1716@gmail.com>
2023-12-21 22:57:09 +0800

    hw5 ok

--- hw5_SFS_FC/hw5..py
+++ hw5_SFS_FC/hw5..py
@@ -58,10 +58,67 @@ def sequential_forward_selection_2fold(X, y):
     return selected_features, accuracies
 
 
-def fisher_criterion_2fold(X, y):
+def fisher_criterion_2fold(X, y, k=30):
     print("------------------------Fisher's Criterion-----------------------------")
 
+    pos_len = len(X[y == 1])
+    neg_len = len(X[y == 0])
+    data_len = (pos_len + neg_len)
 
+    # find the mean vector for each class
+    mean_vectors = []
+    for i in np.unique(y):
+        mean_vectors.append(np.mean(X[y == i], axis=0))
+
+    mean = np.mean(mean_vectors, axis=0)
+
+    # find the within-class matrix (Sw)
+    Sw = np.zeros((X.shape[1], X.shape[1]))
+    for i in np.unique(y):
+        class_samples = X[y == i]
+        for j in range(len(class_samples)):
+            diff = class_samples[j, :] - mean_vectors[i]
+            Sw += np.outer(diff, diff)
+
+    # find the Between-class matrix (Sb)
+    Sb = np.zeros((X.shape[1], X.shape[1]))
+    for i in np.unique(y):
+        Sb += len(X[y == i]) * (mean_vectors[i] - mean) @ (mean_vectors[i] - mean).T
+
+    # calculate Fisher's scores
+    F_scores = np.diag(np.linalg.inv(Sw) @ Sb)
+
+    # sort indices based on F-scores in descending order
+    sorted_indices = np.argsort(F_scores)[::-1]
+
+    # get top k indices and their corresponding F-scores
+    top_k_indices = sorted_indices[:k]
+    top_k_scores = F_scores[top_k_indices]
+
+    # print top k indices and their corresponding F-scores
+    print(f"Top {k} indices: {top_k_indices}")
+
+    for i in range(1, k + 1):
+        selected_features = X[:, top_k_indices[:i]]
+
+
+        fold1_X, fold2_X, fold1_y, fold2_y = train_test_split(selected_features, y, test_size=0.5, random_state=0)
+
+        lda_ = lda.LDA()
+        lda_.fit(fold1_X, fold1_y)
+        fold_1_accuracy = lda_.LDA_decision_function(fold2_X, fold2_y) * 100
+
+        lda_.fit(fold2_X, fold2_y)
+        fold_2_accuracy = lda_.LDA_decision_function(fold1_X, fold1_y) * 100
+
+        accuracy = (fold_1_accuracy + fold_2_accuracy) / 2
+
+        print(f"Top {i} features, Accuracy: {accuracy}")
+        
+
+    return top_k_indices, top_k_scores
+        
+    
 def main():
     cancer = load_breast_cancer()
     x = cancer.data
